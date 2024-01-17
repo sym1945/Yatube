b0fec057a647a97dfb8ac564ef0f9b60ab5d5554
ymshin <sym1945@gmail.com>
2024-01-17 23:46:58 +0900

    sub commit

--- Yatube/Controllers/VideoController.cs
+++ Yatube/Controllers/VideoController.cs
@@ -9,7 +9,7 @@ namespace Yatube.Controllers
     [Route("[controller]")]
     public class VideoController : ControllerBase
     {
-        //
+        // a
         private readonly ILogger<VideoController> _Logger;
         private readonly YatubeContext _Context;
 
